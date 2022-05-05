640388cca6bad2f5a70bfd0f1d1eea5328133bf0
Spencer <Spencerjwheeler@gmail.com>
2022-05-04 20:16:26 -0400

    Leaderboard now updates with inputs from game/total, but doesn't yet have get method

--- Spazzle/leaderboard.py
+++ Spazzle/leaderboard.py
@@ -4,7 +4,60 @@
     Description: Set of API classes for post/get methods for game information
 """
 import sqlite3
-from User import User
 from flask_restful import Resource, reqparse
 #using reqparse despite its depreciated status
+
+class leaderboard(Resource):
+
+    parser = reqparse.RequestParser()
+    
+    '''
+        Table:
+            Position | Username | Game Level | Game_time | Game_Mode 
+    '''
+    def check_leaderboard(self, username, game_time, game_level, game_mode):
+        '''
+            pulls the table and compares levels/time one by one
+            based on game mode calls methods?
+                if new game time and level higher then replace
+                if new game time or level is higher compare further?
+                    
+                if new game time is less than time and level move on
+            if faster/higher level bubble sort the others down
+            
+            put the game in teh new opening
+        '''
+        table_name = game_mode+'_leaderboard'
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        
+        
+        query = '''SELECT * FROM {table} ORDER BY position ASC'''.format(table = table_name)
+        rows = cursor.execute(query).fetchall()
+
+    def get(self):
+        
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        
+        table_name = "level_leaderboard"
+        
+        query = '''SELECT * FROM {table} ORDER BY position ASC'''.format(table = table_name)
+        rows = cursor.execute(query).fetchall()
+        
+        return rows
+    """
+        add a time aspect to the original post that logs a time doesn't need to be called
+        then do it again on the final post that compares the time to the back end timer +/- some
+        time on either side. 
+        
+        When doing the leaderboard jusst do top 10, it should naturally scale from there. 
+    """
+    
+    
+        
+
+    
+    
+
 
