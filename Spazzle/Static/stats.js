b0715a68f5774fdbaec34109970ff3fb1e739e59
Ben P <bdpaul91@gmail.com>
2022-05-07 16:25:02 -0400

    Stats js/css/html. I think everything is working.

--- Spazzle/Static/stats.js
+++ Spazzle/Static/stats.js
@@ -4,60 +4,27 @@
  * Description: Change stats on stats page
  */
 
-/*
- * API CALL GUIDE
- * /average/total = average time of all games
- * /average/<gametype> = average time of individual puzzles
- * /average/addition = not working
- * /game_count/total = number of games played
- */
-
 // Send user back home
 $("#back_button").click(function() {
     window.location.href = '/';
 });
 
 (async function() {
-
+   
     // Display error message and return home
-    async function statsError(error) {
+    function statsError(error) {
         if (error === 'error') {
             alert('Unable to read username. Please play a game to register a username. Returning home.');
         } else if (error === 'Please Register First') {
             alert('Username doesn\'t exist. Please play a game to register a username. Returning home.');
         } else {
-            alert('⚠ Connection error: ' + error.status + ' - ' + error.statusText + ' Returning home.');
+            alert('⚠ Connection error: ' + error.status + ' - ' + error.statusText + '. Returning home.');
         }
         window.location.href = '/';
     }
 
-    // Read username from cookies
-    function readUser() {
-        let getCookies = decodeURIComponent(document.cookie);
-        if (getCookies === '') {
-            statsError('error');
-            return '';
-        }
-        let userMatch = getCookies.match(/username=(.*?)(;|$)/);
-        if (userMatch !== null && userMatch[1] !== undefined && userMatch[1] !== '') {
-            return userMatch[1];
-        } else {
-            statsError('error');
-            return '';
-        }
-    }
-    // Set username
-    const username = readUser();
-    $("#user_heading").text(username);
-    // Check if the username isn't in the database
-    let registered = Object.values(await fetchData('/stats/' + username + '/average/total', 'GET'))[0];
-    if (registered === 'Please Register First') {
-        return statsError(registered);
-    }
-
     // Fetch game data
     async function fetchData(url, method, data) {
-        console.log(data);
         let options = {
             method
         };
@@ -98,6 +65,32 @@ $("#back_button").click(function() {
         return response;
     }
 
+    // Read username from cookies
+    function readUser() {
+        let getCookies = decodeURIComponent(document.cookie);
+        if (getCookies === '') {
+            statsError('error');
+            return '';
+        }
+        let userMatch = getCookies.match(/username=(.*?)(;|$)/);
+        if (userMatch !== null && userMatch[1] !== undefined && userMatch[1] !== '') {
+            return userMatch[1];
+        } else {
+            statsError('error');
+            return '';
+        }
+    }
+    // Set username
+    const username = readUser();
+    $("#user_heading").text(username);
+    // Check if the username isn't in the database
+    let registeredResponse = await fetchData('/stats/' + username + '/total_runs/average/total', 'GET');
+    let registered = Object.values(registeredResponse)[0];
+    if (registered !== undefined && registered.Message === 'Please Register First') {
+        statsError(registered.Message);
+        return;
+    }
+
     // Turn seconds into time string text
     function formatTime(time) {
         let seconds = time;
@@ -132,135 +125,201 @@ $("#back_button").click(function() {
     const statsRow = '<tr class="stats_row"><th class="stats_cell">--</th><th class="stats_cell">--</th><th class="stats_cell">--</th></tr>';
 
     // Modify stat values
-    function changeStats(data, labelSelector, labelText, statSelector, time) {
-        // data is empty when error and null when missing
-        if (data === '' || data === null) {
+    function changeStats(data, labelSelector, labelText, statSelector, type) {
+        // Data is empty when fetch returns an error
+        if (data === '') {
             return;
         }
+        // Get data value from {key: value}
         let dataValue = Object.values(data)[0];
-        if (dataValue === 'Stat not supported') {
-            return;
+        // Fill value with '--' if not a stat
+        if (dataValue === 'Stat not supported' || dataValue === 'Not a supported game type' || dataValue === null) {
+            dataValue = '--';
         }
+        // If selectors don't exist in the table, create new rows, then assign selectors
         if (labelSelector.length === 0 && statSelector.length === 0) {
             statsTable.append(statsRow);
             statsCells = $(".stats_cell");
             let cellsLength = statsCells.length;
             labelSelector = statsCells.eq(cellsLength - 3);
-            statSelector = statsCells.eq(cellsLength - 1);
+            if (type === 'count') {
+                statSelector = statsCells.eq(cellsLength - 2);
+            } else {
+                statSelector = statsCells.eq(cellsLength - 1);
+            }
+        }
+        // Highest fetch call returns array as value, get the level and time
+        console.log(dataValue);
+        if (labelText === 'HIGHEST LEVEL' && dataValue !== '--') {
+            dataValue = dataValue[2];
+        }
+        if (labelText === 'HIGHEST LEVEL TIME' && dataValue !== '--') {
+            dataValue = dataValue[3];
         }
         labelSelector.text(labelText);
-        dataValue = Math.round(dataValue);
-        if (time === 'seconds') {
-            dataValue = formatTime(dataValue);
-        }
-        if (time === 'milliseconds') {
-            dataValue += ' ms'
+        // Format data value
+        if (dataValue !== '--') {
+            dataValue = Math.round(dataValue);
+            if (type === 'seconds') {
+                dataValue = formatTime(dataValue);
+            }
+            if (type === 'milliseconds') {
+                dataValue += ' ms'
+            }
         }
         statSelector.text(dataValue);
     }
 
-    // API calls for Stat Highlights
-    changeStats(await fetchData('/stats/' + username + '/average/total', 'GET'),
-        highlightsTitles.eq(0),
-        'AVERAGE GAME TIME',
-        highlightsNumbers.eq(0),
-        'seconds');
-    changeStats(await fetchData('/stats/' + username + '/game_count/total', 'GET'),
-        highlightsTitles.eq(1),
-        'GAMES PLAYED',
-        highlightsNumbers.eq(1),
-        '');
+    // Gets stats based on mode selected
+    async function callStatsForMode(mode) {
+        // For if mode is not all modes, offset the index because 'levels' isn't called
+        let offset = 0;
+        if (mode !== 'total_runs') {
+            offset = 3;
+        }
 
-    // API calls for Stats Table
-    changeStats(await fetchData('/stats/' + username + '/average/color', 'GET'),
-        statsCells.eq(0),
-        'Color Time',
-        statsCells.eq(2),
-        'milliseconds');
-    changeStats(await fetchData('/stats/' + username + '/average/sort', 'GET'),
-        statsCells.eq(3),
-        'Sort Time',
-        statsCells.eq(5),
-        'milliseconds');
-    changeStats(await fetchData('/stats/' + username + '/average/image', 'GET'),
-        statsCells.eq(6),
-        'Image Time',
-        statsCells.eq(8),
-        'milliseconds');
-    changeStats(await fetchData('/stats/' + username + '/average/addition', 'GET'),
-        statsCells.eq(9),
-        'Addition Time',
-        statsCells.eq(11),
-        'milliseconds');
-    changeStats(await fetchData('/stats/' + username + '/average/word', 'GET'),
-        statsCells.eq(12),
-        'Word Time',
-        statsCells.eq(14),
-        'milliseconds');
-    changeStats(await fetchData('/stats/' + username + '/average/number', 'GET'),
-        statsCells.eq(15),
-        'Number Time',
-        statsCells.eq(17),
-        'milliseconds');
-    //await fetchData('/stats/' + username + '/average/level');
+        // ************************ API Calls For Stats Highlights *************************
+        // Average time of all game runs (seconds)
+        changeStats(await fetchData('/stats/' + username + '/' + mode + '/average/total', 'GET'),
+            highlightsTitles.eq(0),
+            'AVERAGE GAME TIME',
+            highlightsNumbers.eq(0),
+            'seconds');
+        // Number of games played
+        changeStats(await fetchData('/stats/' + username + '/' + mode + '/count/total', 'GET'),
+            highlightsTitles.eq(1),
+            'GAMES PLAYED',
+            highlightsNumbers.eq(1),
+            'count');
+        // Highest level reached - returns {"highest": [game_run, game_mode, level, time]}
+        changeStats(await fetchData('/stats/' + username + '/' + mode + '/highest/total', 'GET'),
+            highlightsTitles.eq(2),
+            'HIGHEST LEVEL',
+            highlightsNumbers.eq(2),
+            'count');
+        // Highest level time - returns {"highest": [game_run, game_mode, level, time]}
+        changeStats(await fetchData('/stats/' + username + '/' + mode + '/highest/total', 'GET'),
+            highlightsTitles.eq(3),
+            'HIGHEST LEVEL TIME',
+            highlightsNumbers.eq(3),
+            'seconds');
 
-    /*
-     * DROPDOWN DOESN'T WORK YET. USE THIS TO TEST VARIOUS API CALLS INSTEAD.
-     */
+        // ************************ API Calls For Stats Table *************************
+        // ********************************** COUNTS **********************************
+        if (mode === 'total_runs') {
+            changeStats(await fetchData('/stats/' + username + '/' + mode + '/count/levels', 'GET'),
+                statsCells.eq(0),
+                'Levels',
+                statsCells.eq(1),
+                'count');
+        }
+        changeStats(await fetchData('/stats/' + username + '/' + mode + '/count/all_games', 'GET'),
+            statsCells.eq(3 - offset),
+            'All Puzzles',
+            statsCells.eq(4 - offset),
+            'count');
+        changeStats(await fetchData('/stats/' + username + '/' + mode + '/count/color', 'GET'),
+            statsCells.eq(6 - offset),
+            'Color Matcher',
+            statsCells.eq(7 - offset),
+            'count');
+        changeStats(await fetchData('/stats/' + username + '/' + mode + '/count/sort', 'GET'),
+            statsCells.eq(9 - offset),
+            'Token Sorter',
+            statsCells.eq(10 - offset),
+            'count');
+        changeStats(await fetchData('/stats/' + username + '/' + mode + '/count/image', 'GET'),
+            statsCells.eq(12 - offset),
+            'Image Match',
+            statsCells.eq(13 - offset),
+            'count');
+        changeStats(await fetchData('/stats/' + username + '/' + mode + '/count/addition', 'GET'),
+            statsCells.eq(15 - offset),
+            'Speed Addition',
+            statsCells.eq(16 - offset),
+            'count');
+        changeStats(await fetchData('/stats/' + username + '/' + mode + '/count/word', 'GET'),
+            statsCells.eq(18 - offset),
+            'Word Scramble',
+            statsCells.eq(19 - offset),
+            'count');
+        changeStats(await fetchData('/stats/' + username + '/' + mode + '/count/number', 'GET'),
+            statsCells.eq(21 - offset),
+            'Numbered Order',
+            statsCells.eq(22 - offset),
+            'count');
+
+        // ********************************** AVERAGES **********************************
+        if (mode === 'total_runs') {
+            changeStats(await fetchData('/stats/' + username + '/' + mode + '/average/levels', 'GET'),
+                statsCells.eq(0),
+                'Levels',
+                statsCells.eq(2),
+                'milliseconds');
+        }
+        changeStats(await fetchData('/stats/' + username + '/' + mode + '/average/all_games', 'GET'),
+            statsCells.eq(3 - offset),
+            'All Puzzles',
+            statsCells.eq(5 - offset),
+            'milliseconds');
+        changeStats(await fetchData('/stats/' + username + '/' + mode + '/average/color', 'GET'),
+            statsCells.eq(6 - offset),
+            'Color Matcher',
+            statsCells.eq(8 - offset),
+            'milliseconds');
+        changeStats(await fetchData('/stats/' + username + '/' + mode + '/average/sort', 'GET'),
+            statsCells.eq(9 - offset),
+            'Token Sorter',
+            statsCells.eq(11 - offset),
+            'milliseconds');
+        changeStats(await fetchData('/stats/' + username + '/' + mode + '/average/image', 'GET'),
+            statsCells.eq(12 - offset),
+            'Image Match',
+            statsCells.eq(14 - offset),
+            'milliseconds');
+        changeStats(await fetchData('/stats/' + username + '/' + mode + '/average/addition', 'GET'),
+            statsCells.eq(15 - offset),
+            'Speed Addition',
+            statsCells.eq(17 - offset),
+            'milliseconds');
+        changeStats(await fetchData('/stats/' + username + '/' + mode + '/average/word', 'GET'),
+            statsCells.eq(18 - offset),
+            'Word Scramble',
+            statsCells.eq(20 - offset),
+            'milliseconds');
+        changeStats(await fetchData('/stats/' + username + '/' + mode + '/average/number', 'GET'),
+            statsCells.eq(21 - offset),
+            'Numbered Order',
+            statsCells.eq(23 - offset),
+            'milliseconds');
+    }
+    // Fill page with stats on page load
+    await callStatsForMode('total_runs');
+
+    // Dropdown changes stats based on mode
     $("#stat_select").change(async function() {
+        // Reset values before calls
+        highlightsTitles.text('--');
+        highlightsNumbers.eq(0).text('0:00:00');
+        highlightsNumbers.eq(1).text('00');
+        highlightsNumbers.eq(2).text('00');
+        highlightsNumbers.eq(3).text('0:00:00');
+        statsCells.text('--');
         let optionSelected = $(this).val();
-        //let statsResponse;
-        let statsData;
         switch (optionSelected) {
             case '1':
-                statsData = await fetchData('/stats/' + username + '/average', 'GET');
-                console.log(statsData);
-                /*
-                await statsResponse.json().then(function(data) {
-                    console.log(data);
-                    statsJSON = data;
-                    console.log(statsJSON);
-                });
-                */
-                //changeStats(statsData);
+                await callStatsForMode('total_runs');
                 break;
             case '2':
-                statsData = await fetchData('/stats/' + username + '/count', 'GET');
-                console.log(statsData);
-                /*
-                await statsResponse.json().then(function(data) {
-                    console.log(data);
-                    statsJSON = data;
-                    console.log(statsJSON);
-                });
-                */
-                //changeStats(statsData);
+                await callStatsForMode('speed');
                 break;
             case '3':
-                statsData = await fetchData('/stats/' + username + '/average/total', 'GET');
-                console.log(statsData);
-                /*
-                await statsResponse.json().then(function(data) {
-                    statsJSON = data;
-                    console.log(statsJSON);
-                });
-                */
-                //changeStats(statsData);
+                await callStatsForMode('level');
                 break;
             case '4':
-                statsData = await fetchData('/stats/' + username + '/count/total', 'GET');
-                console.log(statsData);
-                /*
-                await statsResponse.json().then(function(data) {
-                    statsJSON = data;
-                    console.log(statsResponse);
-                });
-                */
-                //changeStats(statsData);
+                await callStatsForMode('infinite');
                 break;
         }
-        console.log(optionSelected);
-        /* Change stats based on option selected */
     });
 
 })();
